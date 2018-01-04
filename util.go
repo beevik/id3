32298ff6a2c0ddb0b38b10d03c2dc02167ca06ca
Brett Vickers <brett@beevik.com>
2018-01-03 22:15:51 -0800

    Add encodeStrings and decodeStrings helpers.

--- util.go
+++ util.go
@@ -5,6 +5,13 @@ import (
 	"unicode/utf8"
 )
 
+var null = map[Encoding][]byte{
+	EncodingUTF8:     []byte{0},
+	EncodingISO88591: []byte{0},
+	EncodingUTF16:    []byte{0, 0},
+	EncodingUTF16BOM: []byte{0, 0},
+}
+
 // Read a sync-safe uint32 from a buffer of 4 or 5 bytes.
 func readSyncSafeUint32(b []byte) (value uint32, err error) {
 	l := len(b)
@@ -41,51 +48,78 @@ func writeSyncSafeUint32(b []byte, value uint32) error {
 
 // Decode a string stored in a byte slice.
 func decodeString(b []byte, enc Encoding) (string, error) {
+	s, _, err := decodeStringHelper(b, enc)
+	return s, err
+}
+
+// Decode one or more null-terminated strings stored in a byte slice.
+func decodeStrings(b []byte, enc Encoding) ([]string, error) {
+	ss := make([]string, 0, 1)
+	for len(b) > 0 {
+		s, c, err := decodeStringHelper(b, enc)
+		if err != nil {
+			return nil, err
+		}
+		ss = append(ss, s)
+		b = b[c:]
+	}
+	return ss, nil
+}
+
+func decodeStringHelper(b []byte, enc Encoding) (s string, consumed int, err error) {
+	consumed = len(b)
+
 	switch enc {
 	case EncodingISO88591:
 		runes := make([]rune, 0, len(b))
-		for _, c := range b {
+		for i, c := range b {
 			if c == 0 {
+				consumed = i
 				break
 			}
 			runes = append(runes, rune(c))
 		}
-		return string(runes), nil
+		return string(runes), consumed, nil
 
 	case EncodingUTF16BOM:
 		fallthrough
 
 	case EncodingUTF16:
+		start := 0
 		if len(b) >= 2 && b[0] == 0xfe && b[1] == 0xff {
-			b = b[2:]
+			start = 2
 		}
 		if (len(b) & 1) != 0 {
-			return "", ErrBadText
+			return "", 0, ErrBadText
 		}
-		u := make([]uint16, len(b)/2)
-		for i, j := 0, 0; i < len(b); i, j = i+2, j+1 {
-			u[j] = uint16(b[i])<<8 | uint16(b[i+1])
-			if u[j] == 0 {
-				u = u[:j]
+		u := make([]uint16, 0, len(b)/2)
+		j := 0
+		for i := start; i < len(b); i += 2 {
+			cp := uint16(b[i])<<8 | uint16(b[i+1])
+			if cp == 0 {
+				consumed = i + 1
 				break
 			}
+			u = append(u, cp)
+			j++
 		}
-		return string(utf16.Decode(u)), nil
+		return string(utf16.Decode(u)), consumed, nil
 
 	case EncodingUTF8:
 		for i := 0; i < len(b); i++ {
 			if b[i] == 0 {
 				b = b[:i]
+				consumed = i
 				break
 			}
 		}
 		if !utf8.Valid(b) {
-			return "", ErrBadText
+			return "", 0, ErrBadText
 		}
-		return string(b), nil
+		return string(b), consumed, nil
 
 	default:
-		return "", ErrBadText
+		return "", 0, ErrBadText
 	}
 }
 
@@ -122,4 +156,21 @@ func encodeString(s string, enc Encoding) ([]byte, error) {
 	default:
 		return nil, ErrBadText
 	}
+}
+
+// Encode an array of stings to a byte slice. Separate multiple strings
+// by appropriate null terminators.
+func encodeStrings(ss []string, enc Encoding) ([]byte, error) {
+	buf := make([]byte, 0)
+	for i, s := range ss {
+		b, err := encodeString(s, enc)
+		if err != nil {
+			return nil, err
+		}
+		if i > 0 {
+			buf = append(buf, null[enc]...)
+		}
+		buf = append(buf, b...)
+	}
+	return buf, nil
 }
