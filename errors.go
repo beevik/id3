5dac2e35b68efd0f841030d4aece8ab7133906d4
Brett Vickers <brett@beevik.com>
2018-01-24 19:18:19 -0800

    Move files.

---
+++ errors.go
@@ -0,0 +1,22 @@
+package id3
+
+import (
+	"errors"
+)
+
+// Possible errors returned by this package.
+var (
+	ErrInvalidTag           = errors.New("invalid id3 tag")
+	ErrInvalidVersion       = errors.New("invalid id3 version")
+	ErrInvalidHeaderFlags   = errors.New("invalid header flags")
+	ErrBadSync              = errors.New("invalid sync code")
+	ErrBadEncoding          = errors.New("invalid encoding type")
+	ErrBadText              = errors.New("invalid text string encountered")
+	ErrIncompleteFrame      = errors.New("frame truncated prematurely")
+	ErrUnknownFrameType     = errors.New("unknown frame type")
+	ErrInvalidEncoding      = errors.New("invalid text encoding")
+	ErrInvalidFrame         = errors.New("invalid frame structure")
+	ErrInvalidFrameHeader   = errors.New("invalid frame header")
+	ErrInvalidFrameFlags    = errors.New("invalid frame flags")
+	ErrInvalidEncodedString = errors.New("invalid encoded string")
+)
--- tag.go
+++ tag.go
@@ -2,25 +2,7 @@ package id3
 
 import (
 	"bytes"
-	"errors"
 	"io"
-)
-
-// Possible errors returned by this package.
-var (
-	ErrInvalidTag           = errors.New("invalid id3 tag")
-	ErrInvalidVersion       = errors.New("invalid id3 version")
-	ErrInvalidHeaderFlags   = errors.New("invalid header flags")
-	ErrBadSync              = errors.New("invalid sync code")
-	ErrBadEncoding          = errors.New("invalid encoding type")
-	ErrBadText              = errors.New("invalid text string encountered")
-	ErrIncompleteFrame      = errors.New("frame truncated prematurely")
-	ErrUnknownFrameType     = errors.New("unknown frame type")
-	ErrInvalidEncoding      = errors.New("invalid text encoding")
-	ErrInvalidFrame         = errors.New("invalid frame structure")
-	ErrInvalidFrameHeader   = errors.New("invalid frame header")
-	ErrInvalidFrameFlags    = errors.New("invalid frame flags")
-	ErrInvalidEncodedString = errors.New("invalid encoded string")
 )
 
 // A Tag represents an entire ID3 tag, including zero or more frames.
